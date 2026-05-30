e41c98398f44e927329ef023b0866f38082ab79e
amirmalik798 <exception.handler.798@gmail.com>
2026-05-30 20:37:01 +0200

    Update server.js
    Testing installation of ffmpeg in railway

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,21 @@ import app from './app.js';
 import 'dotenv/config';
 import { initializeStorage } from './utils/storage.util.js';
 
+import { exec } from 'child_process';
+
+exec('ffmpeg -version', (error, stdout, stderr) => {
+    console.log('========== FFMPEG TEST ==========');
+
+    if (error) {
+        console.error('FFmpeg NOT FOUND');
+        console.error(error);
+        return;
+    }
+
+    console.log('FFmpeg FOUND');
+    console.log(stdout);
+});
+
 const PORT = process.env.PORT || 3000;
 
 try {
