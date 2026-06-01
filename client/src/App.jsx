ccc3a86cb5d7621548da8a99f95ef87760683a39
amirmalik798 <exception.handler.798@gmail.com>
2026-06-01 13:14:19 +0200

    Update App.jsx Made TailwindCSS changes and refactoring

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,22 +6,21 @@ import ErrorSection from "./components/ErrorSection";
 import SuccessSection from "./components/SuccessSection";
 import { useVideoProcessor } from "./hooks/useVideoProcessor";
 
+
 function App() {
   
   const videoProcessor = useVideoProcessor();
 
   return (
-    <>
-    <div className='min-h-screen bg-slate-100 flex justify-center items-start py-8'>
-      
-      <div className='bg-white rounded-xl shadow-lg p-8 w-full max-w-2xl space-y-6'>
+    <div className='min-h-screen bg-gradient-to-br from-slate-700 via-slate-800 to-gray-950'>
+      <div className='max-w-2xl mx-auto px-4 py-8 space-y-6'>
         <div className='text-center space-y-2'>
-        <h1 className='text-2xl font-bold text-center'>
-          Video Processing Platform
-        </h1>
-        <p className='text-slate-500'>
-          Compress, convert and process videos instantly
-        </p>
+          <h1 className='text-2xl md:text-3xl font-bold text-amber-400'>
+            Video Processing Platform
+          </h1>
+          <p className='text-base md:text-xl text-slate-400'>
+            Convert and process videos instantly
+          </p>
         </div>
 
         <UploadSection {...videoProcessor } />
@@ -36,13 +35,13 @@ function App() {
 
         <DownloadSection {...videoProcessor } />
       
-        <p className='text-m text-center'>Developed by: Amir Malik</p>
+        <p className='text-base md:text-xl text-center text-slate-500 pt-4'>Developed by: Amir Malik</p>
       
       </div>
 
     </div>
-    </>
+  
   )
 }
 
-export default App
+export default App;
