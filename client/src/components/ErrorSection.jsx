c616a583a72fde26783c05411af214374355d682
amirmalik798 <exception.handler.798@gmail.com>
2026-06-01 17:16:04 +0200

    Update ErrorSection.jsx improved color scheme for better UX

--- client/src/components/ErrorSection.jsx
+++ client/src/components/ErrorSection.jsx
@@ -5,8 +5,7 @@ const ErrorSection = (videoProcessor) => {
     }
 
     return (
-        <div className='bg-red-50 border border-red-300 text-red-700
-        p-3 rounded-lg'>
+        <div className='border border-red-500 text-red-400 p-2 rounded-lg'>
             {videoProcessor.error}
         </div>
     )
