b556cbf4d77cfadcd8f997a440e25104c450a1f3
amirmalik798 <exception.handler.798@gmail.com>
2026-06-01 17:06:07 +0200

    Update ProgressSection.jsx improved color scheme for uploading and processing progress

--- client/src/components/ProgressSection.jsx
+++ client/src/components/ProgressSection.jsx
@@ -8,15 +8,15 @@ const ProgressSection = (videoProcessor) => {
         return (
             <div className='space-y-2'>
                 <div className='flex justify-between'>
-                    <span className='font-medium'>
+                    <span className='font-medium text-green-500'>
                         Uploading Video...
                     </span>
-                    <span className='text-sm text-slate-600'>
+                    <span className='text-sm text-green-500'>
                         {videoProcessor.uploadProgress}%
                     </span>
                     </div>
-                    <div className='w-full bg-slate-200 rounded-full h-3'>
-                    <div className='bg-green-600 h-3 rounded-full transition-all
+                    <div className='w-full bg-slate-600 rounded-full h-5'>
+                    <div className='bg-green-600 h-5 rounded-full transition-all
                     duration-300'
                     style={{width: `${videoProcessor.uploadProgress}%`}}>
                     </div>
@@ -30,16 +30,16 @@ const ProgressSection = (videoProcessor) => {
         return (
             <div className='space-y-2'>
                 <div className='flex justify-between'>
-                <span className='font-medium'>
+                <span className='font-medium text-amber-500'>
                    {videoProcessor.operation}
                 </span>
                 
-                <span className='text-sm text-slate-600'>
+                <span className='text-sm text-amber-500'>
                     {videoProcessor.progress}%
                 </span>
                 </div>
-                <div className='w-full bg-slate-200 rounded-full h-3'>
-                <div className='bg-blue-600 h-3 rounded-full transition-all
+                <div className='w-full bg-slate-600 rounded-full h-5'>
+                <div className='bg-amber-500 h-5 rounded-full transition-all
                 duration-300'
                 style={{width: `${videoProcessor.progress}%`}}>
                 </div>
